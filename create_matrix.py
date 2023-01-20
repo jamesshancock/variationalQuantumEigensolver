9c8187cb410da09b34d9efb969b442cee6a5b0bb
Craig McNeile <craig.mcneile@plymouth.ac.uk>
2023-01-20 09:28:37 +0000

    update

---
+++ create_matrix.py
@@ -0,0 +1,44 @@
+#
+#  Create the Hamiltonan 
+#
+#
+
+import math
+import cmath
+import random
+import numpy as np
+import scipy
+from scipy import linalg
+from scipy.optimize import minimize
+from numpy import random
+
+
+pauli_0 = np.array([[1, 0],
+                   [0, 1]])
+pauli_1 = np.array([[0, 1],
+                   [1, 0]])
+pauli_2 = np.array([[0, -1j],
+                   [1j, 0]])
+pauli_3 = np.array([[1, 0],
+                   [0, -1]])
+
+#choosing the coefficients for the hamiltonian
+#a = random.rand()
+h = np.array([[0.0, 0.0, 0.0, 0.0],
+             [0.0, 1.0, 0.0, 0.0],
+             [0.0, 0.0, 0.0, 0.0],
+             [0.0, 0.0, 3.4, 0.0]])
+
+
+H = np.zeros((4,4))
+
+pauli = 'pauli'
+
+#generating the hamiltonian
+for k in range(4):
+    for s in range(4):
+        matrix1 = locals()[pauli +'_'+str(k)]
+        matrix2 = locals()[pauli +'_'+str(s)]
+        Pauli_kron = np.kron(matrix1,matrix2)
+        H = H + h[k,s]*Pauli_kron
+        
